c05207a0d27329d440e61eb7f533dab59d6ee1ed
LinhLH <linhlh1>
2024-11-13 11:32:48 +0700

    commit 1

--- Package.swift
+++ Package.swift
@@ -15,9 +15,6 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "MyLibrary2"),
-        .testTarget(
-            name: "MyLibrary2Tests",
-            dependencies: ["MyLibrary2"]),
+            name: "MyLibrary2")
     ]
 )
---
+++ Sources/MyLibrary2/Helper.swift
@@ -0,0 +1,15 @@
+//
+//  Helper.swift
+//  
+//
+//  Created by LinhLH Pro M1 on 13/11/2024.
+//
+
+import UIKit
+
+public class Helper: NSObject {
+    public func doSomething() {
+        print("I am doing something")
+    }
+
+}
