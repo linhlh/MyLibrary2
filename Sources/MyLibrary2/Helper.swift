f04d109cda252a91313d92f866605d2f76a45db7
LinhLH <linhlh1>
2024-11-13 11:38:36 +0700

    commit 2

--- Sources/MyLibrary2/Helper.swift
+++ Sources/MyLibrary2/Helper.swift
@@ -10,6 +10,8 @@ import UIKit
 public class Helper: NSObject {
     public func doSomething() {
         print("I am doing something")
+        print("I am doing 2nd thing")
+        print("I am doing 3nd thing")
     }
 
 }
